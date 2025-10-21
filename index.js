29a6aeabc0673cc9fdca7856a8a8eaf33575b497
kingflirckz22 <Kingflirckz22@gmail.com>
2025-10-21 12:00:57 +0100

    feat: Add CORS support

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 
 // IMPORTANT: Render sets the PORT environment variable.
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 // A simple in-memory list of quotes
 const quotes = [
